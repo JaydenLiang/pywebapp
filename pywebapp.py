5f74a78c2a912c2b5bafcccbb298d6b71a844175
root <root@ip-172-31-31-232.us-west-2.compute.internal>
2017-11-02 18:05:08 +0000

    Update the page output.

--- pywebapp.py
+++ pywebapp.py
@@ -14,7 +14,7 @@ class Default:
         return """<html>
         <title>PyWebApp</title>
         <body bgcolor="cyan">
-        <h1>Hello AWS Pipeline!</h1>
+        <h1>Hello COMP4913 & AWS CodePipeline!</h1>
         <p>Available API methods:</p>
         <ul>
         <li><a href="/api/v1/test">/api/v1/test</a></li>
